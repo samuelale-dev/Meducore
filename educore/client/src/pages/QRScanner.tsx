a0ab378f93c4e11e5eeb8feecfe7c6228af29743
samuelale-dev <samuelalemu105@gmail.com>
2026-05-22 04:35:17 +0300

    Fix import statement for React in QRScanner.tsx

--- educore/client/src/pages/QRScanner.tsx
+++ educore/client/src/pages/QRScanner.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import , { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Html5QrcodeScanner } from 'html5-qrcode';
 
