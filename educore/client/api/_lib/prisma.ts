e90e024739bc4f7f76ed95a193eaa8bd1e317442
samuelale-dev <samuelalemu105@gmail.com>
2026-05-30 18:49:51 +0300

    Update and rename Prisma.ts to prisma.ts

---
+++ educore/client/api/_lib/prisma.ts
@@ -0,0 +1,5 @@
+// @ts-nocheck
+import { PrismaClient } from '@prisma/client';
+const g = globalThis as any;
+export const prisma = g.prisma ?? new PrismaClient();
+if (process.env.NODE_ENV !== 'production') g.prisma = prisma;
